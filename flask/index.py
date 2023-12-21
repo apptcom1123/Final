d53a44e00a7bfd8e7ddccfefbcc603121c764796
wang <bzw1123@gmail.com>
2023-12-21 15:49:58 +0800

    提交的消息

--- flask/index.py
+++ flask/index.py
@@ -1,11 +1,16 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, send_file
+from flask import Flask, flash, request, redirect, url_for, send_file, jsonify
 from werkzeug.utils import secure_filename
+from torch_utils import transforms_voice, get_predicition
 from flask_cors import CORS
 import uuid
 
 UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'mp4','wav'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 app = Flask(__name__)
 CORS(app)
@@ -16,13 +21,32 @@ app.secret_key = uuid.uuid4().hex
 def hello_world():
     return {"content": "hello world"}
 
+@app.route("/predict")
+def predict():
+    file = request.files.get('file')
+    if file is None or file.filename == "":
+        return jsonify({'error':'no file'})
+    if not allowed_file(file.filename):
+        return jsonify({'error':'format not support'})
+    
+    try:
+        voice_bytes=file.read()
+        tensor = transforms_voice(voice_bytes)
+        prediction = get_predicition(tensor)
+        data = {'prediction': prediction.item(), 'class_name': str(prediction.item())}
+        return jsonify(data)
+    except:
+        return jsonify({'error': 'error during prediction'})
+    
+
+
+
+    return jsonify({'result': 1})
+
+
 @app.route("/api/<params>")
 def api(params):
     return {"content": params   }
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/uploads', methods=['GET', 'POST'])
 def upload_file():
