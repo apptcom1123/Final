a5d30bd05cb1a1b57ad53bf2f4e4946c1b84d9c4
xup60521 <xup60521@gmail.com>
2023-12-22 18:05:16 +0800

    fix: signout direct

--- next/src/app/home/layout.tsx
+++ next/src/app/home/layout.tsx
@@ -1,7 +1,13 @@
+import { getServerSession } from "next-auth"
 import { NavBar } from "./_components/NavBar"
 import "./layout.css"
+import { redirect } from "next/navigation"
 
-export default function Layout({children}:{children: React.ReactNode}) {
+export default async function Layout({children}:{children: React.ReactNode}) {
+    const session = await getServerSession()
+    if (!session) {
+        redirect("/")
+    }
     return (
         <main>
             <NavBar />
--- next/src/app/home/page.tsx
+++ next/src/app/home/page.tsx
@@ -1,13 +1,6 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 import "./Home.css"
 
 export default async function Home() {
-
-    const session = await getServerSession()
-    if (!session) {
-        redirect("/")
-    }
 
     return (
         
