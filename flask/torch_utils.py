8e5734f1c1bd558f856e237d3cc3815e30233d6e
xup60521 <xup60521@gmail.com>
2023-12-24 15:33:34 +0800

    feature: responsive web design

---
+++ flask/torch_utils.py
@@ -0,0 +1,19 @@
+# import torch
+# import torch.nn as nn
+# import torchvision.transforms as transforms
+
+
+# loadmodel
+
+PATH="?"
+
+# voice -> tensor
+def transforms_voice(voice_bytes):
+    
+    return voice_bytes
+
+# predict
+def get_predicition(image_tensor):
+    voice = voice.reshpae()
+
+    return voice
