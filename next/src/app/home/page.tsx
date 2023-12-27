04e3c8b993ab0710089b48c05cbcef59d9fa50fe
xup60521 <xup60521@gmail.com>
2023-12-27 17:44:56 +0800

    add home page text/ fix RWD on firefox (fix CSS)

--- next/src/app/home/page.tsx
+++ next/src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import "./Home.css"
 
 export default async function Home() {
@@ -5,7 +6,12 @@ export default async function Home() {
     return (
         
         <div className="home">
-            
+            <div>
+                <h1>AI歌手翻唱</h1>
+                <h3>使用說明</h3>
+                <p>進入<Link href={`/home/upload`}>Upload</Link>頁面上傳想要翻唱的歌曲，上傳成功後便可以調整參數進行轉換，並下載至你的設備。</p>
+                <p>若想要管理之前的上傳紀錄，可以到<Link href={`/home/history`}>History</Link>查看並編輯。</p>
+            </div>
         </div>
         
     )
